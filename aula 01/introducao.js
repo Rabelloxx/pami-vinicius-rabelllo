b48a0be5aba4e29d7588d1115db0229e9fd13f10
Rabello <vini.mz@hotmail.com>
2026-09-03 22:13:15 -0300

    ajusta itens

---
+++ aula 01/introducao.js
@@ -0,0 +1,27 @@
+console.log("Hello my old friend");
+
+var Variavel01 = "Hello";
+let Variavel02 = "Suaves?";
+const Variavel03 = "I am GOOD";
+// Concatenação de variaveis
+console.log(Variavel01 + Variavel02); // sem espaçoes entre as variaveis
+console.log(Variavel01, Variavel02); // insere espaço entre as variaveis 
+
+
+
+Variavel01 = "Hello my friend"
+console.log(Variavel01,Variavel02);
+
+
+
+// Variavel03 = "Variavel do estilo constante não podem receber novo valor que reescreva o original"
+
+
+let Teste = "texto";
+console.log(typeof(Teste));
+Teste = 2;
+console.log (typeof(Teste));
+Teste = {nome: "Rabello", idade: 34};
+console.log(typeof(Teste));
+Teste = [1,2];
+console.log("O resultado do tipo de array é:", typeof(Teste))

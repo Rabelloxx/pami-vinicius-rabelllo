163ec08738b18f5a132819f9e3e887c185de4ca6
Rabello <vini.mz@hotmail.com>
2026-09-03 22:26:33 -0300

    Aula de concatenação

---
+++ aula 01/Matematica.js
@@ -0,0 +1,6 @@
+let v1 = 2;
+let v2 = 3;
+let v3 = "4";
+console.log(v1 + v2); // aqui quando é let ele soma as variaveis
+console.log(v1,v2);// espaçamento entre as variaveis 
+console.log(v1 + v3) // com "" na variavel ele apenas junta as duas variaveis
